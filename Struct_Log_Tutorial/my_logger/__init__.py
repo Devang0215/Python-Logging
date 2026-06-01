926814889587ec2268b0c6dbb1f54e8cc6f6e15b
devangkamdar <devangkamdar65@gmail.com>
2026-06-01 21:43:52 +0530

    Ruff Formating

--- Struct_Log_Tutorial/my_logger/__init__.py
+++ Struct_Log_Tutorial/my_logger/__init__.py
@@ -1,13 +1,16 @@
 from .config import configure_logger
 
-from .logger import get_logger
+from .logger import get_logger as get_logger
 
 from .telemetry import setup_tracing
-
-from .middleware import logging_middleware
 
 from .metrics import router as metrics_router
 
 
 def setup_metrics(app):
     app.include_router(metrics_router)
+
+
+configure_logger()
+
+setup_tracing()
