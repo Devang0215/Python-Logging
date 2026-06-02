c50660e05ea541be3a12e0cea8d12dfc7fffa701
Devang Kamdar <devangkamdar65@gmail.com>
2026-06-02 11:17:47 +0530

    Again Formatting

--- Struct_Log_Tutorial/my_logger/__init__.py
+++ Struct_Log_Tutorial/my_logger/__init__.py
@@ -1,27 +1,13 @@
 from .config import configure_logger
-<<<<<<< HEAD
 from .logger import get_logger as get_logger
-=======
-
-from .logger import get_logger as get_logger
-
-from .telemetry import setup_tracing
-
->>>>>>> 926814889587ec2268b0c6dbb1f54e8cc6f6e15b
 from .metrics import router as metrics_router
 from .telemetry import setup_tracing
-
 
 
 def setup_metrics(app):
     app.include_router(metrics_router)
 
 
-<<<<<<< HEAD
-setup_tracing()
-configure_logger()
-=======
 configure_logger()
 
 setup_tracing()
->>>>>>> 926814889587ec2268b0c6dbb1f54e8cc6f6e15b
