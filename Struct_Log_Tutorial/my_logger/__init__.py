d0222686c353acf1dbe2790179a0d76c8cadf86e
Devang Kamdar <devangkamdar65@gmail.com>
2026-06-02 19:09:23 +0530

    Modified middleware

--- Struct_Log_Tutorial/my_logger/__init__.py
+++ Struct_Log_Tutorial/my_logger/__init__.py
@@ -2,12 +2,11 @@ from .config import configure_logger
 from .logger import get_logger as get_logger
 from .metrics import router as metrics_router
 from .telemetry import setup_tracing
-
+from .middleware import logging_middleware
 
 def setup_metrics(app):
     app.include_router(metrics_router)
 
 
 configure_logger()
-
 setup_tracing()
